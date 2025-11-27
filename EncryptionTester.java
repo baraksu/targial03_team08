f8528b4b0b97de601edecba7975114968d0173cc
MatanH5 <matan.hoffert@gmail.com>
2025-11-27 18:10:10 +0200

    methods 1, 2 tests

--- EncryptionTester.java
+++ EncryptionTester.java
@@ -10,12 +10,43 @@ import org.junit.Test;
  */
 public class EncryptionTester
 {
-    //@Test
-    /*public void testAdd(){
+    @Test
+    //methods 1 tests (matan)
+    public void testAdd(){
         
-        int result = Encryption.add(2,3);
-        assertEquals(5,result);
+        int result = Encryption.wordsNum("aaa bbb ccc");
+        assertEquals(3,result);
         
     }
-    */
+    public void testAdd1(){
+        
+        int result = Encryption.wordsNum("aaa bbb");
+        assertEquals(2,result);
+        
+    }
+    public void testAdd2(){
+        
+        int result = Encryption.wordsNum("aaa");
+        assertEquals(1,result);
+        
+    }
+    // methods 2 tests (matan)
+    public void testAdd3(){
+        
+        String result = Encryption.newPassword("aaa",1);
+        assertEquals("aaa",result);
+        
+    }
+    public void testAdd4(){
+        
+        String result = Encryption.newPassword("aaa bbb",2);
+        assertEquals("bbb aaa",result);
+        
+    }
+    public void testAdd5(){
+        
+        String result = Encryption.newPassword("aaa bbb ccc",3);
+        assertEquals("ccc aaa bbb",result);
+        
+    }
 }
